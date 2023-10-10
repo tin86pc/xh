9e65a0580a133052751d7c2afa962f58aea71271
tu m4800 <tinpc07@gmail.com>
2023-10-10 16:30:13 +0700

    chinh sua giao dien

--- main.js
+++ main.js
@@ -19,8 +19,6 @@ function TaiXuong() {
     downloadURI(dataUrl, 'qrcode.png');
 }
 
-
-
 function downloadURI(uri, name) {
     var link = document.createElement("a");
     link.download = name;
@@ -31,23 +29,4 @@ function downloadURI(uri, name) {
     delete link;
 };
 
-// window.onload = function () {
-//     console.log('onload');
-//     let qrcode = new QRCode(document.getElementById("qrcode"),
-//         {
-//             text: "http://www.runoob.com",
-//             width: 450,
-//             height: 450,
-//             colorDark: "#000000",
-//             colorLight: "#ffffff",
-//             correctLevel: QRCode.CorrectLevel.H
-//         });
-//     setTimeout(
-//         function () {
-//             let dataUrl = document.querySelector('#qrcode').querySelector('img').src;
-//             downloadURI(dataUrl, 'qrcode.png');
-//         }
-//         , 1000);
-
-// };
 
